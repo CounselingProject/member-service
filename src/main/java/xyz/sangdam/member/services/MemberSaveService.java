f5202ed7863ed8a19bf39b3e5fb491f142f19de2
parksaehyun <psh666777@naver.com>
2024-09-11 17:34:53 +0900

    fix : memberSaveService 빠진 form 수정 추가

--- src/main/java/xyz/sangdam/member/services/MemberSaveService.java
+++ src/main/java/xyz/sangdam/member/services/MemberSaveService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import xyz.sangdam.member.MemberUtil;
+import xyz.sangdam.member.constants.Gender;
 import xyz.sangdam.member.constants.Status;
 import xyz.sangdam.member.constants.UserType;
 import xyz.sangdam.member.controllers.RequestJoin;
@@ -61,20 +62,22 @@ public class MemberSaveService {
         member.setAddress(form.getAddress());
         member.setAddresssub(form.getAddressSub());
         member.setGid(form.getGid());
+        member.setStatus(Status.valueOf(form.getStatus()));
+        member.setGender(Gender.valueOf(form.getGender()));
+        member.setDeptNm(form.getDeptNm());
+        member.setDeptNo(form.getDeptNo());
         /* 공통 항목 처리 E */
 
-        // 상담사 추가 정보
+        // 상담사, 교수, 관리자 추가 정보
         if (member instanceof Employee employee) {
             employee.setEmpNo(form.getEmpNo());
             employee.setSubject(form.getSubject());
-            employee.setStatus(Status.valueOf(form.getStatus()));
+
             employeeRepository.saveAndFlush(employee);
 
         } else if (member instanceof Student student){ // 학생 추가 정보
             student.setStdntNo(form.getStdntNo());
             student.setGrade(form.getGrade());
-            student.setDeptNm(form.getDeptNm());
-            student.setStatus(Status.valueOf(form.getStatus()));
 
             studentRepository.saveAndFlush(student);
         }
